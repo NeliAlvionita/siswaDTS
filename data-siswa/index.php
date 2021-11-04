ae835b76956df2df013dbfab9c7da3969d61758c
Evina08 <=>
2021-11-04 21:48:15 +0700

    cek

--- data-siswa/index.php
+++ data-siswa/index.php
@@ -1,30 +1,29 @@
-<?php  
+<?php
 
 include '../auth.php';
 include '../koneksi.php';
 
 $role = "";
 
-$id 	= $_SESSION['auth'];
+$id     = $_SESSION['auth'];
 
 
 if ($_SESSION['role_user'] == 0) {
-	$role = "Admin";
+    $role = "Admin";
     $query      = "SELECT * FROM akun WHERE id = $id";
 
     $exec       = mysqli_query($conn, $query);
 
     if ($exec) {
-    
+
         while ($user = mysqli_fetch_array($exec)) {
-            foreach ($user as $key=>$val) {
+            foreach ($user as $key => $val) {
                 ${$key} = $val;
-            }       
+            }
         }
     }
-
-}else{
-	$role = "User";
+} else {
+    $role = "User";
     $query      = "SELECT a.*,b.* FROM pendaftaran a, akun b WHERE a.id = $id AND b.id_user=$id";
 
     $exec       = mysqli_query($conn, $query);
@@ -32,9 +31,9 @@ if ($_SESSION['role_user'] == 0) {
 
     if ($exec) {
         while ($user = mysqli_fetch_array($exec)) {
-            foreach ($user as $key=>$val) {
+            foreach ($user as $key => $val) {
                 ${$key} = $val;
-            }       
+            }
         }
     }
 }
@@ -45,31 +44,31 @@ if ($_SESSION['role_user'] == 0) {
 $getPage = $_GET['page'];
 
 switch ($getPage) {
-	case 1:
-		$page 				= "home.php";
-		$_SESSION['active']	= "1";
-		break;
-	case 2:
-		$page 				= "data_siswa.php";
-		$_SESSION['active']	= "2";
-		break;
-	case 3:
-		$page 				= "edit_profile.php";
-		$_SESSION['active']	= "2";
-		break;
-	case 4:
-		$page 				= "status_pendaftaran.php";
-		$_SESSION['active'] = "3";
-		break;
-	case 5:
-		$page 				= "upload_nilai.php";
-		$_SESSION['active'] = "3";
-		break;
-	
-	case 6:
-		$page 				= "../data_admin/data_pendaftaran.php";
-		$_SESSION['active']	= "4";
-		break;
+    case 1:
+        $page                 = "home.php";
+        $_SESSION['active']    = "1";
+        break;
+    case 2:
+        $page                 = "data_siswa.php";
+        $_SESSION['active']    = "2";
+        break;
+    case 3:
+        $page                 = "edit_profile.php";
+        $_SESSION['active']    = "2";
+        break;
+    case 4:
+        $page                 = "status_pendaftaran.php";
+        $_SESSION['active'] = "3";
+        break;
+    case 5:
+        $page                 = "upload_nilai.php";
+        $_SESSION['active'] = "3";
+        break;
+
+    case 6:
+        $page                 = "../data_admin/data_pendaftaran.php";
+        $_SESSION['active']    = "4";
+        break;
     case 7:
         $page               = "../data_admin/detail_pendaftaran.php";
         $ida                = $_GET['ida'];
@@ -83,48 +82,48 @@ switch ($getPage) {
         $_SESSION['active'] = "4";
         break;
     case 9:
-        $page 				= "../data_admin/laporan.php";
-        $_SESSION['active']	= "5";
-        break;  
+        $page                 = "../data_admin/laporan.php";
+        $_SESSION['active']    = "5";
+        break;
     case 10:
-        $page 				= "../data_admin/data_user.php";
-        $_SESSION['active']	= "6";
-        break;   
-        case 11:
-            $page 				= "cetak_bukti.php";
-            $_SESSION['active']	= "2";
-            break; 
-	default:
-		$page 	= "home.php";
-		$_SESSION['active']	= "1";
-		break;
+        $page                 = "../data_admin/data_user.php";
+        $_SESSION['active']    = "6";
+        break;
+    case 11:
+        $page                 = "cetak_bukti.php";
+        $_SESSION['active']    = "2";
+        break;
+    default:
+        $page     = "home.php";
+        $_SESSION['active']    = "1";
+        break;
 }
 
 ?>
 
 <!doctype html>
 <html lang="en">
-	
+
 <head>
-    
+
     <title>Dashboard <?php echo $role; ?></title>
-	
+
 
     <meta charset="utf-8" />
-<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png" />
-<link rel="icon" type="image/png" href="../assets/img/favicon.png" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-<meta name="viewport" content="width=device-width" />
-<!-- Bootstrap core CSS     -->
-<link href="https://objectstorage.ap-sydney-1.oraclecloud.com/n/sdrwwjsjidrf/b/bucket-uts/o/css%2Fbootstrap.min.css" rel="stylesheet" />
-<!--  Material Dashboard CSS    -->
-<link href="https://objectstorage.ap-sydney-1.oraclecloud.com/n/sdrwwjsjidrf/b/bucket-uts/o/css%2Fmaterial-dashboard.css" />
-<!--  CSS for Demo Purpose, don't include it in your project     -->
-<link href="https://objectstorage.ap-sydney-1.oraclecloud.com/n/sdrwwjsjidrf/b/bucket-uts/o/css%2Fdemo.css" />
-<!--     Fonts and icons     -->
-<link href="https://objectstorage.ap-sydney-1.oraclecloud.com/n/sdrwwjsjidrf/b/bucket-uts/o/css%2Ffont-awesome.min.css" rel="stylesheet">
-<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png" />
+    <link rel="icon" type="image/png" href="../assets/img/favicon.png" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+    <!-- Bootstrap core CSS     -->
+    <link href="https://objectstorage.ap-sydney-1.oraclecloud.com/n/sdrwwjsjidrf/b/bucket-uts/o/css%2Fbootstrap.min.css" rel="stylesheet" />
+    <!--  Material Dashboard CSS    -->
+    <link href="../assets/css/material-dashboard.css?v=1.2.0" rel="stylesheet" />
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="../assets/css/demo.css" rel="stylesheet" />
+    <!--     Fonts and icons     -->
+    <link href="../assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
 
 
 </head>
@@ -150,61 +149,61 @@ switch ($getPage) {
                             <p>Home</p>
                         </a>
                     </li>
-					
-					<?php  
-					if ($role == "User") {
-					?>
-					<li class="<?php $_SESSION['active'] == 2 ? print("active") : print("") ?>">
-                        <a href="index.php?page=2">
-                            <i class="material-icons">person</i>
-                            <p>User Profile </p>
-                        </a>
-                    </li>
-                    <li class="<?php $_SESSION['active'] == 2 ? print("active") : print("") ?>">
-                        <a href="index.php?page=11">
-                            <i class="material-icons">person</i>
-                            <p>Cetak Bukti </p>
-                        </a>
-                    </li>
-					<?php
-					}
-					?>
 
-                    
-                    <?php  
-					if ($role == "Admin") {
-					?>
-					<li class="<?php $_SESSION['active'] == 4 ? print("active") : print("") ?>">
-                        <a href="index.php?page=6">
-                            <i class="material-icons">content_paste</i>
-                            <p>Konfirmasi Pendaftaran </p>
-                        </a>
-                    </li>
-                    <li class="<?php $_SESSION['active'] == 5 ? print("active") : print("") ?>">
-                        <a href="index.php?page=9">
-                            <i class="material-icons">content_paste</i>
-                            <p>Laporan </p>
-                        </a>
-                    </li>
-                    <li class="<?php $_SESSION['active'] == 6 ? print("active") : print("") ?>">
-                        <a href="index.php?page=10">
-                            <i class="material-icons">content_paste</i>
-                            <p>Data User </p>
-                        </a>
-                    </li>
-					<?php
-					}
-					?>
-                    
-                    <?php  
+                    <?php
                     if ($role == "User") {
                     ?>
-                    <li class="<?php $_SESSION['active'] == 3 ? print("active") : print("") ?>">
-                        <a href="index.php?page=4">
-                            <i class="material-icons">content_paste</i>
-                            <p>Syarat Pendaftaran</p>
-                        </a>
-                    </li>
+                        <li class="<?php $_SESSION['active'] == 2 ? print("active") : print("") ?>">
+                            <a href="index.php?page=2">
+                                <i class="material-icons">person</i>
+                                <p>User Profile </p>
+                            </a>
+                        </li>
+                        <li class="<?php $_SESSION['active'] == 2 ? print("active") : print("") ?>">
+                            <a href="index.php?page=11">
+                                <i class="material-icons">person</i>
+                                <p>Cetak Bukti </p>
+                            </a>
+                        </li>
+                    <?php
+                    }
+                    ?>
+
+
+                    <?php
+                    if ($role == "Admin") {
+                    ?>
+                        <li class="<?php $_SESSION['active'] == 4 ? print("active") : print("") ?>">
+                            <a href="index.php?page=6">
+                                <i class="material-icons">content_paste</i>
+                                <p>Konfirmasi Pendaftaran </p>
+                            </a>
+                        </li>
+                        <li class="<?php $_SESSION['active'] == 5 ? print("active") : print("") ?>">
+                            <a href="index.php?page=9">
+                                <i class="material-icons">content_paste</i>
+                                <p>Laporan </p>
+                            </a>
+                        </li>
+                        <li class="<?php $_SESSION['active'] == 6 ? print("active") : print("") ?>">
+                            <a href="index.php?page=10">
+                                <i class="material-icons">content_paste</i>
+                                <p>Data User </p>
+                            </a>
+                        </li>
+                    <?php
+                    }
+                    ?>
+
+                    <?php
+                    if ($role == "User") {
+                    ?>
+                        <li class="<?php $_SESSION['active'] == 3 ? print("active") : print("") ?>">
+                            <a href="index.php?page=4">
+                                <i class="material-icons">content_paste</i>
+                                <p>Syarat Pendaftaran</p>
+                            </a>
+                        </li>
                     <?php
                     }
                     ?>
@@ -215,26 +214,26 @@ switch ($getPage) {
                             <p>Logout</p>
                         </a>
                     </li>
-                   
+
                 </ul>
             </div>
         </div>
         <div class="main-panel">
             <div class="content">
                 <div class="container-fluid">
-                   
-                   <?php  
 
-                   include $page;
+                    <?php
 
-                   ?>
-                        
+                    include $page;
+
+                    ?>
+
                 </div>
             </div>
-            
+
         </div>
     </div>
-    
+
 </body>
 
 <!--   Core JS Files   -->
@@ -257,8 +256,8 @@ switch ($getPage) {
 <script src="../assets/js/demo.js"></script>
 
 <script>
-    $(document).ready(function(){
-        $("#cetak").click(function(){
+    $(document).ready(function() {
+        $("#cetak").click(function() {
             window.print();
         });
     });
