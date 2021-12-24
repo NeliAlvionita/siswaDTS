abe2e81ff53a1546a8e812867769aaa4596864eb
NeliAlvionita <nelialvionita123@gmail.com>
2021-12-24 10:40:28 +0000

    uas

--- koneksi.php
+++ koneksi.php
@@ -3,9 +3,10 @@
 error_reporting(0);
 
 //Database Configuration
-$db_name 	= "dts_psb";
-$host		= "localhost";
-$username	= "root";
+$db_name 	= "MYSQL_DATABASE";
+$host		= "db";
+$username	= "MYSQL_USER";
+$password       = "MYSQL_PASSWORD";
 
 
 //make connection to database
